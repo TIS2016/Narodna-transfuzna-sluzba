0fe9023cf442b6dad702f93e5b6ac62aa3222772
Samuel Mitas <s.mitas@devinprint.sk>
2016-11-08 16:29:16 +0100

    Remove bootstrap and init materialize-css

--- isnts/views.py
+++ isnts/views.py
@@ -3,6 +3,4 @@ from django import forms
 
 
 def home(request):
-    class NameForm(forms.Form):
-        your_name = forms.CharField(label='Your name', max_length=100)
-    return render(request, 'home.html', {"form": NameForm()})
+    return render(request, 'donor_login.html')
