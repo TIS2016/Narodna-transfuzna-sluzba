5b3da07e5c54d4ad2e65395cfcf8d3b68bf218ea
Jakub Trubač <Jakub19942@gmail.com>
2016-11-25 14:35:16 +0100

    Separate login and regitration
    Closes #6

--- isnts/forms.py
+++ isnts/forms.py
@@ -21,3 +21,8 @@ class Register(forms.ModelForm):
     class Meta:
         model = DonorCard
         fields = ['first_name', 'last_name', 'username', 'email', 'password', 'gender']
+
+class PassChange(forms.Form):
+    old_password = forms.CharField()
+    new_password = forms.CharField()
+    new_password2 = forms.CharField()
--- isnts/urls.py
+++ isnts/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
     url(r'^donors/$', views.donor_listview),
     url(r'^login/$', views.donor_login),
     url(r'^logout/$', views.donor_logout),
+    url(r'^register/$', views.donor_register),
+    url(r'^pass_change/$', views.donor_pass_change),
     url(r'^donors/information/$', views.donor_information),
     url(r'^$', views.home)
 ]
