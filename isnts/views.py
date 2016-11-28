57c21e2b2e2db6629e036cdb10e93f934d0a6896
Samuel Mitas <mitas.samuel@gmail.com>
2016-11-28 03:14:40 +0100

    Fix max-len of line

--- isnts/views.py
+++ isnts/views.py
@@ -152,7 +152,11 @@ def blood_extraction_listview(request):
     samples_new = BloodExtraction.objects.filter(state=0)
     samples_ready_for_exp = BloodExtraction.objects.filter(state=1)
     samples_shipped = BloodExtraction.objects.filter(state=2)
-    return render(request, 'blood_extraction/listview.html', {'samples_new': samples_new, 'samples_ready_for_exp': samples_ready_for_exp, 'samples_shipped': samples_shipped})
+    return render(request, 'blood_extraction/listview.html', {
+        'samples_new': samples_new,
+        'samples_ready_for_exp': samples_ready_for_exp,
+        'samples_shipped': samples_shipped
+    })
 
 
 def blood_extraction_detailview(request, blood_extraction_id):
