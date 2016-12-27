b825c712b92e333006ec9d1d8c77862da1bb4c29
Samuel Mitas <mitas.samuel@gmail.com>
2016-12-27 14:38:28 +0100

    Set max-len of towns name to 255

--- isnts/models.py
+++ isnts/models.py
@@ -67,7 +67,7 @@ class Region(models.Model):
 
 
 class Town(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=250)
     id_region = models.ForeignKey(Region, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
