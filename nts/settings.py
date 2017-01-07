e915d62ebd5e49cf5b1bfb4fd5e7882abc8a0ae3
Samuel Mitas <mitas.samuel@gmail.com>
2017-01-07 01:50:57 +0100

    Remove translation related comment

--- nts/settings.py
+++ nts/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
-#from django.utils.translation import ugettext_lazy as _
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -43,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'isnts',
     'jquery',
-    'nocaptcha_recaptcha',
+    'nocaptcha_recaptcha'
 ]
 
 MIDDLEWARE_CLASSES = [
